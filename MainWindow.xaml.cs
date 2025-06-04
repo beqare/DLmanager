527ef714741b24236c430794172d58322e577ff2
beqare <turtelmaster11@gmail.com>
2025-06-04 20:39:02 +0200

    Revert "Upgraded to .NET 9.0"
    This reverts commit d0c0b9044615125d8d66e121e9292c9b7776f6e2.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace DLmanager
