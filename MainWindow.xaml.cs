d0c0b9044615125d8d66e121e9292c9b7776f6e2
beqare <turtelmaster11@gmail.com>
2025-06-04 20:36:51 +0200

    Upgraded to .NET 9.0

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace DLmanager
